b4889ca43416bd0483041df4b6a3f45895886e64
s905060 <s905060@gmail.com>
2014-11-11 16:01:38 -0500

    Get rid of PATH
    Get rid of PATH

--- vm_controller.py
+++ vm_controller.py
@@ -5,15 +5,15 @@ __author__ = 'Jash Lee'
 #Get argument
 parser = argparse.ArgumentParser(description='This is a script to manipulate VM')
 parser.add_argument('-c','--Command', help='Command for VM',required=True)
-parser.add_argument('-p','--Path', help='Path to the VM',required=True)
+#parser.add_argument('-p','--Path', help='Path to the VM',required=True)
 parser.add_argument('-n','--Name', help='Name of the VM',required=True)
 args = parser.parse_args()
 
 #Run command line tool
 if str(args.Command) in ("create", "destroy", "shutdown", "suspend", "resume", "autostart"):
 	try:
-		print 'virsh', str(args.Command), str(args.Path) + str(args.Name)
-		subprocess.check_call(['virsh', str(args.Command), str(args.Path) + str(args.Name)])
+		print 'virsh', str(args.Command), str(args.Name)
+		subprocess.check_call(['virsh', str(args.Command), str(args.Name)])
 	except Exception as e:
 		print e
 else:
